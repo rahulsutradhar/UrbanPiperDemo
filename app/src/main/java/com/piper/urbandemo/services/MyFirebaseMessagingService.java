3e3563d2be4ff0be77d5b9cbd2c9679339bffb25
rahulthebox <rahul@thebox.one>
2017-12-11 09:01:34 +0530

    feat: notification working

--- app/src/main/java/com/piper/urbandemo/services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/piper/urbandemo/services/MyFirebaseMessagingService.java
@@ -9,4 +9,9 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        
+    }
 }
